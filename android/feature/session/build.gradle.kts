9d85a28aa1d10393d149578153d1081d6861df3f
article <article@163.com>
2026-07-10 17:35:28 +0800

    fix: 统一所有模块compileSdk=37修复CI构建失败

--- android/feature/session/build.gradle.kts
+++ android/feature/session/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.openmate.feature.session"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
--- android/feature/settings/build.gradle.kts
+++ android/feature/settings/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.openmate.feature.settings"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
