47f326d6b0577a83be192b3dc6f1b9073071eed1
article <article@163.com>
2026-09-25 18:00:58 +0800

    release: v0.3.6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.openmate"
         minSdk = 26
         targetSdk = 36
-        versionCode = 25
-        versionName = "0.3.5"
+        versionCode = 26
+        versionName = "0.3.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
